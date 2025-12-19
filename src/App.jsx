82254112e14749e7876ea6fbdab6364cc2f8d816
Gebu19 <gebu830@gmail.com>
2025-12-19 14:31:07 +0700

    added react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Greetings from './components/Greeting'
+function App() {
+  
+
+  return (
+    <>
+      <div className='card'> 
+        <Greetings message='This content is rendered on the browser'/> 
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+import "../App.css" 
+function Greetings({message}){
+    return(
+        <>
+            <h2>{message}</h2>
+        </>
+    );
+}
+export default Greetings;
